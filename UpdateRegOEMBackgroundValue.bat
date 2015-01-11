f42e28d61f0f85380933147dda9b34610a2106d0
Joe Chiang <joe@chiang.com.tw>
2015-01-12 00:03:58 +0800

    input directory

--- UpdateRegOEMBackgroundValue.bat
+++ UpdateRegOEMBackgroundValue.bat
@@ -1,6 +1,10 @@
+@echo off
+set /p var=Input File Directory:
+cd "%var%"
 reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Background" 
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Background" /f /v "OEMBackground" /t REG_DWORD /d "1"
 reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Background" 
 md C:\Windows\System32\oobe\info
 md C:\Windows\System32\oobe\info\backgrounds
-xcopy "C:\Users\JoeC\Dropbox\Documents\Coding\CustomWindowsLoginBackground\backgroundDefault.jpg" "C:\Windows\System32\oobe\info\backgrounds"
+del "C:\Windows\System32\oobe\info\backgrounds\backgroundDefault.jpg"
+xcopy "%cd%\backgroundDefault.jpg" "C:\Windows\System32\oobe\info\backgrounds"
